e24a17b8e22905b470e66459785be1be54b0781a
Vikas Predhva <vp.official666@gmail.com>
2024-01-29 17:58:18 +0530

    Initial commit

---
+++ src/main/java/com/heroku/RestController.java
@@ -0,0 +1,20 @@
+package com.heroku;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@org.springframework.web.bind.annotation.RestController
+public class RestController {
+
+    @GetMapping("/welcome")
+    public String welcome(){
+        return "Welcome to Heroku app";
+    }
+
+    @GetMapping("/user/{user}")
+    public String welcomeUser(@PathVariable String  user){
+        return "Welcome "+user+" user to Heroku app";
+    }
+
+}
